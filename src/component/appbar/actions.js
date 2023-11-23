6da523643c49563d4a1e0e8adf088ba39363ba2d
Nikshitha Shetty <nikshitha.s@sonata-software.com>
2023-11-23 13:38:39 +0530

    Update on login

--- src/component/appbar/actions.js
+++ src/component/appbar/actions.js
@@ -4,11 +4,39 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Colors } from "../../public/styles/theme";
 import { ActionIconsContainerDesktop , ActionIconsContainerMobile ,MyList} from "../../public/styles/appbar";
-
+import React, { useState, useEffect } from 'react';
+import LogoutIcon from '@mui/icons-material/Logout';
 export default function Actions({ matches }) {
 
   const Component = matches ? ActionIconsContainerMobile : ActionIconsContainerDesktop;
+  const [userData, setUserData] = useState(null);
 
+  useEffect(() => {
+    // Check for user data in localStorage on initial mount
+    const storedUserData = localStorage.getItem('userData');
+    const userData = storedUserData ? JSON.parse(storedUserData) : null;
+    setUserData(userData);
+  
+    // Add the event listener to handle user logins
+    const handleUserLoggedIn = (event) => {
+      const storedUserData = localStorage.getItem('userData');
+      const userData = storedUserData ? JSON.parse(storedUserData) : null;
+      setUserData(userData);
+    };
+  
+    window.addEventListener('userLoggedIn', handleUserLoggedIn);
+  
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('userLoggedIn', handleUserLoggedIn);
+    };
+  }, []); // Empty dependency array to run the effect only on mount
+  const handleLogout = () => {
+    // Clear user data from localStorage
+    localStorage.removeItem('userData');
+    // Clear user data from component state
+    setUserData(null);
+  };
   return (
     <Component>
       <MyList type="row">
@@ -49,6 +77,12 @@ export default function Actions({ matches }) {
             justifyContent: "center",
           }}
         >
+          {userData ? (
+        <div>
+          <p>Welcome, {userData.username}!</p>
+          <button onClick={handleLogout}><LogoutIcon/></button>
+        </div>
+      ) : (
           <a href="/signin"><ListItemIcon 
             sx={{
               display: "flex",
@@ -58,6 +92,7 @@ export default function Actions({ matches }) {
           >
             <PersonIcon />
           </ListItemIcon></a>
+        )}
         </ListItemButton>
         <Divider orientation="vertical" flexItem />
       </MyList>
